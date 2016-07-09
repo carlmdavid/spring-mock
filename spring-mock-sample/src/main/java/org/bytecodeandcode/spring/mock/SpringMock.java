41a7de2aeec0326fd1753d04c59229be06d46c84
Carl <carlmichaeldavid2@gmail.com>
2016-07-09 19:15:28 -0400

    Add main method

--- spring-mock-sample/src/main/java/org/bytecodeandcode/spring/mock/SpringMock.java
+++ spring-mock-sample/src/main/java/org/bytecodeandcode/spring/mock/SpringMock.java
@@ -1,8 +1,12 @@
 package org.bytecodeandcode.spring.mock;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMock {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringMock.class, args);
+	}
 
 }
